784a2c8850e86dc00f07c6fabe73bd5efe53deb5
Andrey Cañon <36200299+andrey-canon@users.noreply.github.com>
2025-10-09 11:01:10 -0500

    feat: add frontend-essentials package and implement ProfileDataModal

--- src/learning-header/LearningHeader.jsx
+++ src/learning-header/LearningHeader.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { getConfig } from '@edx/frontend-platform';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { AppContext } from '@edx/frontend-platform/react';
+import { ProfileDataModal } from '@edunext/frontend-essentials';
 
 import AnonymousUserMenu from './AnonymousUserMenu';
 import AuthenticatedUserDropdown from './AuthenticatedUserDropdown';
@@ -43,6 +44,7 @@ const LearningHeader = ({
           <AuthenticatedUserDropdown
             username={authenticatedUser.username}
           />
+          <ProfileDataModal />
         </>
         )}
         {showUserDropdown && !authenticatedUser && (
