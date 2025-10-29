8efc9b0c90bc9e716cd8d052fb1b15382b0e19de
Andrey Cañon <36200299+andrey-canon@users.noreply.github.com>
2025-10-29 12:08:56 -0500

    feat: add profile data modal to desktop header

--- src/desktop-header/DesktopHeader.jsx
+++ src/desktop-header/DesktopHeader.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { getConfig } from '@edx/frontend-platform';
+import { ProfileDataModal } from '@edunext/frontend-essentials';
 
 // Local Components
 import DesktopUserMenuToggleSlot
@@ -80,6 +81,7 @@ const DesktopHeader = ({
                 <>
                   {renderSecondaryMenu()}
                   {renderUserMenu()}
+                  <ProfileDataModal />
                 </>
               ) : renderLoggedOutItems()}
           </nav>
